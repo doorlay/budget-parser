9b4a45cd89266bd5c110c1a3de5ea131f9c14a82
Nic Doorlay <ndoorlay@gmail.com>
2024-08-05 17:41:31 -0700

    Add basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # budget-parser
+A tool to help me do my budget faster by parsing bank statements and grouping transactions into categories.
+
+| Bank                | Supported?          |
+| --------------------| --------------------|
+| Discover            | ✅                  |
+| Chase               | ❌ (in progress)    |
+
+## Setup
+`python3 -m venv .venv`    
+`source .venv/bin/activate`  
+`pip3 install -r requirements.txt`
+
+## Usage
+Put statement PDFs in the "statements" folder, rename them as appropriate.  
+Run `python3 parse.py`
---
+++ parse.py
@@ -0,0 +1,113 @@
+import os
+import pymupdf
+
+DIRECTORY_NAME = "statements"
+
+class Purchase:
+    def __init__(self, description, price):
+        self.description = description 
+        self.price = price
+
+    def __str__(self):
+        return f"Description: {self.description}\nPrice: {self.price}"
+
+
+# Given a string, determines if it's a date
+def is_date(date_string: str) -> bool:
+    split_date_string = date_string.split("/")
+    if split_date_string != date_string:
+        for num in split_date_string:
+            stripped_num = num.strip()
+            if len(stripped_num) != 2 or not stripped_num.isnumeric():
+                return False
+        return True
+    else:
+        return False 
+
+
+# Given the extracted text of a Chase statement, create a list of Purchase objects
+def parse_chase(text) -> list[Purchase]:
+    lines = text[2].split("$ Amount")[1].split("\n")[1:] 
+    purchases = []
+    for i in range(0, len(lines), 3):
+        if is_date(lines[i]):
+            purchases.append(Purchase(lines[i+1], lines[i+2]))
+        else:
+            break
+    return purchases
+
+
+# Given the extracted text of a Discover statement, create a list of Purchase objects
+def parse_discover(text) -> list[Purchase]:
+    lines = text[0].split("Category")[1].split("Statement Balance is the total")[0].split("\n")
+    purchases = []
+    for index, line in enumerate(lines):
+        # If the current line is a date, it is potentially the beginning of a new row
+        if is_date(line):
+            # Beginning of new line has two sequential dates. Confirm that to prevent duplicates
+            if not is_date(lines[index + 1]):
+                continue 
+            offset = 2
+            purchase_description = ""
+            purchase_amount = "" 
+            # Iterate through the remaining columns in the document to grab all purchase data
+            while True:
+                description = lines[index + offset]
+                if "$" in description:
+                    purchase_amount = description[2:].split(" ")[0] 
+                    purchases.append(Purchase(purchase_description, purchase_amount))
+                    break
+                purchase_description += description
+                offset += 1
+    return purchases
+
+
+def extract_text(file_name: str) -> list[str]:
+    doc = pymupdf.open(file_name)
+    text_list = []
+    for page in doc:
+        text_list.append(page.get_text())
+    return text_list
+
+
+def get_categories():
+    categories = dict() 
+    cat_num = 1
+    while True:
+        cat_name = input("Input category name, or 0 when done: ")
+        if cat_name in categories.values():
+            print("Category name in use, try again.")
+            continue
+        elif cat_name == "0":
+            break
+        else:
+            categories[cat_num] = cat_name
+            cat_num += 1
+    return categories
+
+parsed = []
+# For each file in the queue
+for filename in os.listdir(DIRECTORY_NAME):
+    f = os.path.join(DIRECTORY_NAME, filename)
+    if os.path.isfile(f):
+        if "discover" in f:
+            text = extract_text(f)
+            parsed.extend(parse_discover(text))
+            # for p in parsed:
+            #     print(p.description)
+            #     print(p.price)
+        elif "chase" in f:
+            text = extract_text(f)
+            parsed.extend(parse_chase(text))
+            # for p in parsed:
+            #     print(p.description)
+            #       # print(p.price)
+
+categories = get_categories()
+print(f"Categories: {categories}")
+for i in range(len(parsed)):
+    input(f"Enter category for '{parsed[i].description}': ")
+"""
+Desired output, print out categories and amounts.
+The categories will have been configured by me.
+"""
